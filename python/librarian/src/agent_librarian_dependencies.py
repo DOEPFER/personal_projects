a288b04ed64381118cd827d7366e5f8c5874c7dc
DOEPFER <62898461+DOEPFER@users.noreply.github.com>
2026-05-15 13:37:04 -0300

    Delete python/librarian directory

--- python/librarian/src/agent_librarian_dependencies.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-from pathlib import Path
-
-def get_library_shelves(path: Path) -> List[str]:
-    return [shelf.relative_to(path).as_posix() for shelf in path.rglob(pattern='*/')]
